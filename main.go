9d5daa44529a1c7df8e7bad10629881bcded0f9c
hiifong <i@hiif.ong>
2023-09-12 21:13:09 +0800

    Update

--- main.go
+++ main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing"
+)
+
+const repoPath = "./testdata"
 
 func main() {
 	fmt.Println("Hello Gib!")
+	r, err := git.PlainOpen(repoPath)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	branches, err := r.Branches()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	err = branches.ForEach(func(reference *plumbing.Reference) error {
+		fmt.Println("branch name: ", reference.Name().Short())
+		return nil
+	})
+	if err != nil {
+		return
+	}
 }
